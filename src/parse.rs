5ced031bb1439273fd5464e98c14414923a5f06b
Troels Korreman <troels.korreman@gmail.com>
2023-01-09 20:10:50 +0100

    More parser testing

--- src/parse.rs
+++ src/parse.rs
@@ -2,9 +2,9 @@
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{alpha1, alphanumeric1, char, newline, one_of, space0},
-    combinator::{complete, map, map_opt, map_res, not, opt, recognize, value},
-    multi::{many0, many1, many1_count, separated_list0},
+    character::complete::{alpha1, alphanumeric1, char, newline, one_of, satisfy, space0},
+    combinator::{complete, map, map_opt, map_res, opt, recognize, value},
+    multi::{many0, many0_count, many1, separated_list0},
     sequence::{delimited, pair, preceded, terminated, tuple},
     IResult, Parser,
 };
@@ -17,8 +17,21 @@ use crate::ast::*;
 
 // Tokens
 
-fn num_hex(i: &str) -> Res<i64> {
-    map_res(
+fn num(i: &str) -> Res<i64> {
+    let num_dec = map_res(
+        recognize(many1(terminated(one_of("0123456789"), many0(char('_'))))),
+        |out: &str| str::replace(out, "_", "").parse::<i64>(),
+    );
+
+    let num_bin = map_res(
+        preceded(
+            tag("0b"),
+            recognize(many1(terminated(one_of("01"), many0(char('_'))))),
+        ),
+        |out: &str| i64::from_str_radix(&str::replace(out, "_", ""), 2),
+    );
+
+    let num_hex = map_res(
         preceded(
             tag("0x"),
             recognize(many1(terminated(
@@ -27,60 +40,17 @@ fn num_hex(i: &str) -> Res<i64> {
             ))),
         ),
         |out: &str| i64::from_str_radix(&str::replace(out, "_", ""), 16),
-    )(i)
-}
+    );
 
-fn num_bin(i: &str) -> Res<i64> {
-    map_res(
-        preceded(
-            tag("0b"),
-            recognize(many1(terminated(one_of("01"), many0(char('_'))))),
-        ),
-        |out: &str| i64::from_str_radix(&str::replace(out, "_", ""), 2),
-    )(i)
-}
-
-fn num_dec(i: &str) -> Res<i64> {
-    map_res(
-        recognize(many1(terminated(one_of("0123456789"), many0(char('_'))))),
-        |out: &str| str::replace(out, "_", "").parse::<i64>(),
-    )(i)
-}
-
-fn num(i: &str) -> Res<i64> {
     alt((num_hex, num_bin, num_dec))(i)
 }
 
 fn ident(i: &str) -> Res<&str> {
-    recognize(pair(
-        alt((alpha1, tag("_"))),
-        many1_count(alt((alphanumeric1, tag("_")))),
-    ))(i)
-}
-
-// Structural parsing
-
-fn line_sep(i: &str) -> Res<()> {
-    value(
-        (),
-        many1(tuple((
-            space0,
-            opt(pair(char(';'), many0(not(char('\n'))))),
-            newline,
-        ))),
-    )(i)
-}
-
-fn label(i: &str) -> Res<Label> {
-    terminated(ident, tag(":")).map(|l| l.to_owned()).parse(i)
-}
-
-fn line(i: &str) -> Res<Line> {
-    label.map(Line::Label).or(instr.map(Line::Instr)).parse(i)
-}
-
-pub fn module(i: &str) -> Res<Module> {
-    complete(separated_list0(line_sep, line))(i)
+    recognize(tuple((
+        opt(tag("_")),
+        alpha1,
+        many0_count(alt((alphanumeric1, tag("_")))),
+    )))(i)
 }
 
 // Instruction parsing
@@ -106,7 +76,7 @@ fn reg(i: &str) -> Res<Reg> {
 
 fn imm(i: &str) -> Res<Imm> {
     // TODO: Negative numbers
-    alt((num_dec, num_hex, num_bin))(i)
+    num(i)
 }
 
 fn reg_imm(i: &str) -> Res<RegImm> {
@@ -225,4 +195,32 @@ fn instr(i: &str) -> Res<Instr> {
     let exit = value(Instr::Exit, tag("exit"));
     // Missing: LoadMapFd
     alt((unary, binary, load, load_imm, store, jcc, jmp, call, exit))(i)
+}
+
+// Structural parsing
+
+fn line_sep(i: &str) -> Res<()> {
+    value(
+        (),
+        many1(tuple((
+            space0,
+            opt(pair(char(';'), many0(satisfy(|c| c != '\n')))),
+            newline,
+            space0,
+        ))),
+    )(i)
+}
+
+fn label(i: &str) -> Res<Label> {
+    terminated(ident, pair(space0, tag(":")))
+        .map(|l| l.to_owned())
+        .parse(i)
+}
+
+fn line(i: &str) -> Res<Line> {
+    label.map(Line::Label).or(instr.map(Line::Instr)).parse(i)
+}
+
+pub fn module(i: &str) -> Res<Module> {
+    complete(separated_list0(line_sep, line))(i)
 }
--- src/parse/tests.rs
+++ src/parse/tests.rs
@@ -1,92 +1,151 @@
+use std::fmt::Debug;
+
 use super::*;
-use nom::{combinator::eof, sequence::terminated};
+use nom::{combinator::eof, error::ParseError, sequence::terminated};
 
-// TODO: better error messages
-macro_rules! accepts {
-    ( $parser:ident, $input:expr ) => {
-        parses!($parser, $input, $input);
-    };
+fn accepts<'a, T, E: ParseError<&'a str>>(parser: impl Parser<&'a str, T, E>, input: &'a str) {
+    if terminated(parser, eof).parse(input).is_err() {
+        panic!("rejects: {input:?}")
+    }
 }
 
-macro_rules! parses {
-    ( $parser:ident, $input:expr, $output:expr) => {
-        assert_eq!(
-            nom::sequence::terminated($parser, nom::combinator::eof)($input),
-            Ok(("", $output))
-        );
-    };
+fn rejects<'a, T, E: ParseError<&'a str>>(parser: impl Parser<&'a str, T, E>, input: &'a str) {
+    if terminated(parser, eof).parse(input).is_ok() {
+        panic!("accepts: {input:?}")
+    }
 }
 
-macro_rules! rejects {
-    ( $parser:ident, $input:expr ) => {
-        assert!(
-            match nom::sequence::terminated($parser, nom::combinator::eof)($input) {
-                Err(_) => true,
-                _ => false,
-            },
-            "accepted \"{}\"",
-            $input,
-        );
-    };
+fn parses<'a, T: Eq + Debug, E: ParseError<&'a str>>(
+    parser: impl Parser<&'a str, T, E>,
+    input: &'a str,
+    expected: T,
+) {
+    match terminated(parser, eof).parse(input) {
+        Ok((_, res)) => {
+            if res != expected {
+                panic!("\nexpected: {expected:?}\n  actual: {res:?}");
+            }
+        }
+        Err(_) => panic!("rejected: {input:?}"),
+    }
 }
 
 #[test]
 fn numbers() {
     // decimal
-    parses!(num, "0", 0);
-    parses!(num, "1", 1);
-    parses!(num, "9", 9);
-    parses!(num, "39824", 39824);
-    parses!(num, "1234567890", 1234567890);
-    parses!(num, "348_922_30_875_", 34892230875);
+    parses(num, "0", 0);
+    parses(num, "1", 1);
+    parses(num, "9", 9);
+    parses(num, "009", 9);
+    parses(num, "39824", 39824);
+    parses(num, "1234567890", 1234567890);
+    parses(num, "348_922_30_875_", 34892230875);
 
-    rejects!(num, "_123");
-    rejects!(num, "123a");
-    rejects!(num, "123a456");
-    rejects!(num, "a123");
-    rejects!(num, "");
+    rejects(num, "");
+    rejects(num, "_123");
+    rejects(num, "123a");
+    rejects(num, "123a456");
+    rejects(num, "123 456");
+    rejects(num, "-123"); // NOTE: num doesn't itself parse signs
+    rejects(num, "- 123");
 
     // hexadecimal
-    parses!(num, "0x0", 0);
-    parses!(num, "0x1", 1);
-    parses!(num, "0xf", 0xf);
-    parses!(num, "0x1234567890abcdef", 0x1234567890abcdef);
-    parses!(num, "0xdeadbeef", 0xdeadbeef);
-    parses!(num, "0xdE_aD_bE_eF_", 0xdeadbeef);
+    parses(num, "0x0", 0);
+    parses(num, "0x1", 1);
+    parses(num, "0xf", 0xf);
+    parses(num, "0x1234567890abcdef", 0x1234567890abcdef);
+    parses(num, "0xdeadbeef", 0xdeadbeef);
+    parses(num, "0xdE_aD_bE_eF_", 0xdeadbeef);
 
-    rejects!(num, "0x_123");
-    rejects!(num, "_0x123");
-    rejects!(num, "x123");
-    rejects!(num, "0x");
-    rejects!(num, "0xx");
-    rejects!(num, "deadbeef");
+    rejects(num, "0x_123");
+    rejects(num, "_0x123");
+    rejects(num, "x123");
+    rejects(num, "0x");
+    rejects(num, "0xx");
+    rejects(num, "deadbeef");
 
     // binary
-    parses!(num, "0b0", 0);
-    parses!(num, "0b1", 1);
-    parses!(num, "0b11", 3);
-    parses!(num, "0b101010", 42);
-    parses!(num, "0b11010010", 0b11010010);
-    parses!(num, "0b1_1_010_010_", 0b11010010);
+    parses(num, "0b0", 0);
+    parses(num, "0b1", 1);
+    parses(num, "0b11", 3);
+    parses(num, "0b101010", 42);
+    parses(num, "0b11010010", 0b11010010);
+    parses(num, "0b1_1_010_010_", 0b11010010);
 
-    parses!(num, "1_1_010_010_", 11010010);
-    rejects!(num, "0b123");
-    rejects!(num, "0bb");
-    rejects!(num, "0b_101");
-    rejects!(num, "_0b101");
-    rejects!(num, "b101");
-    rejects!(num, "0b");
+    parses(num, "1_1_010_010_", 11010010);
+    rejects(num, "0b123");
+    rejects(num, "0bb");
+    rejects(num, "0b_101");
+    rejects(num, "_0b101");
+    rejects(num, "b101");
+    rejects(num, "0b");
 }
 
 #[test]
 fn identifiers() {
-    accepts!(ident, "identifier");
-    accepts!(ident, "identifier_again");
-    accepts!(ident, "abcdefghijklmnopqrstuvwxyzæøå");
-    accepts!(ident, "_hello_world_");
+    accepts(ident, "identifier");
+    accepts(ident, "identifier_again");
+    accepts(ident, "sphinx_of_black_quartz_judge_my_vow");
+    accepts(ident, "PACK_MY_BOX_WITH_FIVE_DOZEN_LIQOUR_JUGS");
+    accepts(ident, "_hello_WORLD_");
+    accepts(ident, "abc123");
 
-    rejects!(ident, "_");
-    rejects!(ident, "__");
-    rejects!(ident, "________");
-    rejects!(ident, "________");
+    rejects(ident, "");
+    rejects(ident, "_");
+    rejects(ident, "__");
+    rejects(ident, "________");
+    rejects(ident, "123");
+    rejects(ident, "123abc");
+    rejects(ident, "_123");
+    rejects(ident, "_abc ");
+    rejects(ident, " _abc");
+    for c in " `~!@#$%^&*[]+-=()\\{}|;':\",./<>?".chars() {
+        rejects(ident, format!("hello{c}world").as_str());
+    }
+}
+
+fn instructions() {
+}
+
+#[test]
+fn line_separators() {
+    accepts(line_sep, "\n");
+    accepts(line_sep, "    \n");
+    accepts(line_sep, "\t    \n");
+    accepts(line_sep, "    \n   ");
+    accepts(line_sep, ";\n");
+    accepts(line_sep, "   ;\n");
+    accepts(line_sep, "   ;\n   ");
+    accepts(line_sep, ";comment\n");
+    accepts(line_sep, "   ; a comment \n   ");
+    accepts(line_sep, "   ;; ; comments can be unicode: æØ§ŒfïÐ§ÓÍ \n   ");
+    accepts(line_sep, "\n\n\n");
+    accepts(line_sep, "\n;\n\n");
+    accepts(line_sep, "; comment \n\n ;comment   \n");
+
+    rejects(line_sep, "");
+    rejects(line_sep, "a\n");
+    rejects(line_sep, "a \n");
+    rejects(line_sep, "not a comment \n");
+    rejects(line_sep, "\n ; comments have to end with a newline");
+    rejects(line_sep, "\n ; \n; \n;");
+}
+
+#[test]
+fn labels() {
+    let positives = [
+        ("l:", "l"),
+        ("l :", "l"),
+        ("LABEL :", "LABEL"),
+        ("_XYZ123_:", "_XYZ123_"),
+        ("_XYZ123_  \t  :", "_XYZ123_"),
+    ];
+
+    for (input, expect) in positives {
+        parses(label, input, expect.to_owned());
+    }
+
+    rejects(label, ":");
+    rejects(label, "some_identifier");
+    rejects(label, "some identifiers");
 }
