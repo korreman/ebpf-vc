196e1a7aa3c1adbe29dc7b9f13f63e3a3efbe317
Troels Korreman <troels.korreman@gmail.com>
2023-01-13 10:30:22 +0100

    Revamp VC step

--- src/vc.rs
+++ src/vc.rs
@@ -1,6 +1,4 @@
 //! Verification condition generation.
-use std::mem::swap;
-
 mod preprocess;
 pub use preprocess::*;
 
@@ -8,105 +6,114 @@ pub mod ast;
 use crate::{ast::WordSize, logic::*};
 use ast::*;
 
+enum BlockStatus {
+    Untouched,
+    Pending,
+    PreCond(Formula),
+}
+
 pub fn vc(module: Module) -> Option<Vec<Formula>> {
     // Stores results.
     let mut verif_conds: Vec<Formula> = Vec::new();
 
     // Stores cached pre-conditions for each block.
     // Also used to track which blocks have already been visited.
-    let mut pre_conds: Vec<Option<Formula>> = module.blocks.iter().map(|_| None).collect();
-
-    // Create copy of the CFG with the edges reversed.
-    // Pre-emptively cache the pre-assertions as pre-conditions.
-    let mut reverse_graph: Vec<Vec<Label>> = module.blocks.iter().map(|_| Vec::new()).collect();
-    for (label, block) in module.blocks.iter().enumerate() {
-        match block.next {
-            Continuation::Jmp(target) => reverse_graph[target].push(label),
-            Continuation::Jcc(_, _, _, target_t, target_f) => {
-                reverse_graph[target_t].push(label);
-                reverse_graph[target_f].push(label);
-            }
-            Continuation::Exit => {}
-        }
-        if let Some(pre_assert) = &block.pre_assert {
-            pre_conds[label] = Some(pre_assert.clone());
-        }
-    }
-
-    // Collect all blocks that end with an exit.
-    let mut labels: Vec<Label> = module
+    let mut pre_conds: Vec<_> = module
         .blocks
         .iter()
-        .enumerate()
-        .filter(|(_, block)| block.next == Continuation::Exit)
-        .map(|(label, _)| label)
+        .map(|_| BlockStatus::Untouched)
         .collect();
 
     // Perform a reverse breadth-first traversal of CFG.
+    let mut stack = vec![0];
     let mut f = FormulaBuilder::new();
-    let mut next_labels: Vec<usize> = Vec::new();
-    while !labels.is_empty() {
-        for label in labels.drain(..) {
-            let block = &module.blocks[label];
-            // Generate post-condition from continuation.
-            let post_cond = match block.next {
-                Continuation::Jcc(cc, lhs, rhs, target_t, target_f) => {
-                    let lhs = f.reg(lhs).0;
-                    let rhs = match rhs {
-                        RegImm::Reg(r) => f.reg(r).0,
-                        RegImm::Imm(i) => f.val(i),
-                    };
-                    let cond = f.rel(cc, lhs, rhs);
-                    if let (Some(cond_t), Some(cond_f)) =
-                        (&pre_conds[target_t], &pre_conds[target_f])
-                    {
-                        f.or(
-                            f.and(cond.clone(), cond_t.clone()),
-                            f.and(f.not(cond), cond_f.clone()),
-                        )
+    while let Some(label) = stack.pop() {
+        let block = &module.blocks[label];
+        pre_conds[label] = BlockStatus::Pending;
+
+        // Generate post-condition from continuation.
+        // TODO: Generalize retrieval of post-conditions.
+        let post_cond = match block.next {
+            Continuation::Exit => f.top(),
+            Continuation::Jmp(target) => match &pre_conds[target] {
+                BlockStatus::PreCond(c) => c.clone(),
+                BlockStatus::Untouched => {
+                    stack.push(label);
+                    stack.push(target);
+                    continue;
+                }
+                BlockStatus::Pending => {
+                    if let Some(c) = &module.blocks[target].pre_assert {
+                        c.clone()
                     } else {
-                        continue;
+                        return None;
                     }
                 }
-                Continuation::Jmp(target) => pre_conds[target]
-                    .clone()
-                    .expect("block {target} should already have a pre-condition"),
-                Continuation::Exit => f.top(),
-            };
-            // Perform WP-calculus on post-condition with block body.
-            let wp_result = wp(&mut f, &block.body, post_cond);
-            // Cache or use result of WP.
-            if let Some(pre_assert) = &block.pre_assert {
-                // If the block has a pre-assertion,
-                // add a VC requiring that the pre-assertion implies the WP result.
-                let extra_cond = f.implies(pre_assert.clone(), wp_result);
-                verif_conds.push(extra_cond);
-            } else {
-                // If the block doesn't have a pre-assertion,
-                // but has already been visited,
-                // abort.
-                if pre_conds[label].is_some() {
-                    return None;
-                }
-                // Otherwise, cache the WP result for the block.
-                pre_conds[label] = Some(wp_result);
+            },
+            Continuation::Jcc(cc, lhs, rhs, target_t, target_f) => {
+                let cond_t = match &pre_conds[target_t] {
+                    BlockStatus::PreCond(c) => c.clone(),
+                    BlockStatus::Untouched => {
+                        stack.push(label);
+                        stack.push(target_t);
+                        continue;
+                    }
+                    BlockStatus::Pending => {
+                        if let Some(c) = &module.blocks[target_t].pre_assert {
+                            c.clone()
+                        } else {
+                            return None;
+                        }
+                    }
+                };
+                let cond_f = match &pre_conds[target_f] {
+                    BlockStatus::PreCond(c) => c.clone(),
+                    BlockStatus::Untouched => {
+                        stack.push(label);
+                        stack.push(target_f);
+                        continue;
+                    }
+                    BlockStatus::Pending => {
+                        if let Some(c) = &module.blocks[target_f].pre_assert {
+                            c.clone()
+                        } else {
+                            return None;
+                        }
+                    }
+                };
+                let lhs = f.reg(lhs).0;
+                let rhs = match rhs {
+                    RegImm::Reg(r) => f.reg(r).0,
+                    RegImm::Imm(i) => f.val(i),
+                };
+                let cc = f.rel(cc, lhs, rhs);
+                f.or(
+                    f.and(cc.clone(), cond_t.clone()),
+                    f.and(f.not(cc), cond_f.clone()),
+                )
             }
-            // Add blocks that target this one to the next round.
-            next_labels.extend(reverse_graph[label].iter());
+        };
+
+        // Perform WP-calculus on post-condition with block body.
+        let wp_result = wp(&mut f, &block.body, post_cond);
+
+        // Cache or use result of WP.
+        if let Some(pre_assert) = &block.pre_assert {
+            // If the block has a pre-assertion,
+            // add a VC requiring that the pre-assertion implies the WP result.
+            verif_conds.push(f.implies(pre_assert.clone(), wp_result));
+            pre_conds[label] = BlockStatus::PreCond(pre_assert.clone());
+        } else {
+            // Otherwise, cache the WP result for the block.
+            pre_conds[label] = BlockStatus::PreCond(wp_result);
         }
-        // Prepare labels for next round.
-        next_labels.sort();
-        next_labels.dedup();
-        swap(&mut labels, &mut next_labels);
     }
+
     // Add the pre-condition of the starting block as a VC.
-    verif_conds.push(
-        pre_conds
-            .into_iter()
-            .next()
-            .expect("empty input")
-            .expect("didn't reach start"),
-    );
+    verif_conds.push(match &pre_conds[0] {
+        BlockStatus::PreCond(c) => c.clone(),
+        _ => panic!("starting block never resolved"),
+    });
     Some(verif_conds)
 }
 
